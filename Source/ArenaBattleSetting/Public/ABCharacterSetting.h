7b86219f87f18d65b6b4ae194b6d6deb5f2e5984
victoryAshe <80302657+victoryAshe@users.noreply.github.com>
2024-02-21 16:12:41 +0900

    Fix: StageSelect blur 제거

---
+++ Source/ArenaBattleSetting/Public/ABCharacterSetting.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ABCharacterSetting.generated.h"
+
+/**
+ * 
+ */
+UCLASS(config=ArenaBattle)
+class ARENABATTLESETTING_API UABCharacterSetting : public UObject
+{
+	GENERATED_BODY()
+	
+
+};
